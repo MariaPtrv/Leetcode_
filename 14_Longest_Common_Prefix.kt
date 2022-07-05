c5e5fc5e5a6c1058d8b1a91c73f32610e049b3c2
Maria <58878188+MariaPtrv@users.noreply.github.com>
2022-07-05 23:51:32 +0300

    Create 14_Longest_Common_Prefix.kt

---
+++ 14_Longest_Common_Prefix.kt
@@ -0,0 +1,20 @@
+//14. Longest Common Prefix
+//https://leetcode.com/problems/longest-common-prefix/submissions/
+
+class Solution {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        if (strs.size == 0) return ""
+        var len = 0
+        var res = ""
+        for (j in 0 until strs[0].length) {
+            val c = strs[0][j]
+            for (i in 1 until strs.size) {
+                if (j >= strs[i].length || strs[i][j] != c) {
+                    return res
+                }
+            }
+            res += c
+        }
+        return res
+    }
+}
