607931a5d722d0b543828e5ea3be47f89c40d174
Maria <58878188+MariaPtrv@users.noreply.github.com>
2022-07-21 11:35:46 +0300

    Create 283_Move_Zeroes.js

---
+++ 283_Move_Zeroes.js
@@ -0,0 +1,19 @@
+//283. Move Zeroes
+//https://leetcode.com/problems/move-zeroes/
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    return nums.sort((a,b)=>{
+    if (a==0 && b!==0)
+        return 1
+    
+    if (a!=0 && b==0)
+        return -1
+    
+    if ((a!=0 && b!=0)|| (a==0 && b==0))
+        return 0
+    
+ })};
