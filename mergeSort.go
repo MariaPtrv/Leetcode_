8857bfb11ea0b5728476bcd46de2f4e597b702c6
Maria <58878188+MariaPtrv@users.noreply.github.com>
2024-07-25 13:33:48 +0300

    add mergeSort

---
+++ mergeSort.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{3, 4, 1, 43, 643, 21, 332, 53, -2, 90, 0}
+	fmt.Println(arr)
+	fmt.Println("Sorted: ", mergeSort(arr))
+}
+
+func mergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	var merged []int
+	for len(left) > 0 && len(right) > 0 {
+		if left[0] <= right[0] {
+			merged = append(merged, left[0])
+			left = left[1:]
+		} else {
+			merged = append(merged, right[0])
+			right = right[1:]
+		}
+	}
+
+	merged = append(merged, left...)
+	merged = append(merged, right...)
+
+	return merged
+}
